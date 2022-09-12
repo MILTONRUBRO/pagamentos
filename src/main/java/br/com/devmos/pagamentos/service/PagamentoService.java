4f4e634078bcb35889b7e9a2aa901f282704a745
Milton <miltonrubro@gmail.com>
2022-09-12 20:21:38 -0300

    Adicionando metodo para busca de um pagamento pelo id

--- src/main/java/br/com/devmos/pagamentos/service/PagamentoService.java
+++ src/main/java/br/com/devmos/pagamentos/service/PagamentoService.java
@@ -1,12 +1,15 @@
 package br.com.devmos.pagamentos.service;
 
 import br.com.devmos.pagamentos.dto.PagamentoDTO;
+import br.com.devmos.pagamentos.model.Pagamento;
 import org.modelmapper.ModelMapper;
 import br.com.devmos.pagamentos.repository.PagamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 public class PagamentoService {
@@ -20,5 +23,10 @@ public class PagamentoService {
 
     public Page<PagamentoDTO> getPagamentos(Pageable paginacao){
         return pagamentoRepository.findAll(paginacao).map( p -> modelMapper.map(p , PagamentoDTO.class));
+    }
+
+    public PagamentoDTO getPagamentosById(Long id){
+        Pagamento pagamento = pagamentoRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+        return modelMapper.map(pagamento, PagamentoDTO.class);
     }
 }
